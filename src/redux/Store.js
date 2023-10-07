da400e24b33d34c328214977f95daa3d8f6d2ff5
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-10-07 19:34:50 +0300

    add

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -3,11 +3,43 @@ import { accountReducer } from '../components/AccountSlice';
 import { localeReducer } from '../components/LocaleReducer';
 import { configureStore } from '@reduxjs/toolkit';
 
+const myMd1 = store => next => action => {
+    console.log('myMd1', action)
+    next(action)
+};
+
+const myMd2 = store => next => action => {
+    console.log('myMd2', action)
+    next(action)
+};
+
+// function myMd1(store) {
+//     return function(next) {
+//         return function (action) {
+//             console.log('myMd1', action)
+//             next(action)
+//         }
+//     }
+// }
+
+// function myMd2(store) {
+//     return function(next) {
+//         return function (action) {
+//             console.log('myMd2', action)
+//             next(action)
+//         }
+//     }
+// }
+
 export const store = configureStore({
     reducer: {
         account: accountReducer,
         locale: localeReducer,
-    }
+    },
+    middleware: getDefaultMiddleware => {
+        const defaultMd = getDefaultMiddleware();
+        return [...defaultMd, myMd1, myMd2]
+}
 });
 
 
